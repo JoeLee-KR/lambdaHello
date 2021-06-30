8bbbe1395df354249af95646badb6ab2be8c633f
Jisu Lee <jisu.lee@dicolabs.kr>
2021-06-30 17:07:23 +0900

    DoogieNyx:210630-1707

--- myVanilla/js/config.js
+++ myVanilla/js/config.js
@@ -1,7 +1,6 @@
 var configConstants = {
 	auth0: {
 		domain: 'lambda-xjoelee.us.auth0.com',
-		clientId: 'ZgmFQjPL6TdaBmqfgmHycWpMaZ3aw2rl'
-	},
-  apiBaseUrl: 'https://API-GATEWAY-URL/dev'
+		clientId: 'KUn3zjgNkOSYOBXaHK7JOYHJGUWc3NxY'
+	}
 };
