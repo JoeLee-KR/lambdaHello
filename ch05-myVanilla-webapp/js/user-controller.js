54bf7d576c7531e119e794895ba418d6ca96d3fd
JoeLee-KR <xjoelee@gmail.com>
2021-07-08 23:13:08 +0900

    DoogieJoe:210708-2313

--- ch05-myVanilla-webapp/js/user-controller.js
+++ ch05-myVanilla-webapp/js/user-controller.js
@@ -1,3 +1,11 @@
+/**
+ * Created by Peter Sbarski
+ * Serverless Architectures on AWS
+ * http://book.acloud.guru/
+ * Last Updated: Feb 11, 2017
+ * Modified by CSDN, Auth0 new feature, Custom by Joe
+ */
+
 window.userController = {
   data: {
     auth0Lock: null,
@@ -13,6 +21,8 @@ window.userController = {
   },
 
   init(config) {
+    var that = this;
+
     {
       const { uiElements } = this;
       uiElements.loginButton = $("#auth0-login");
@@ -27,35 +37,50 @@ window.userController = {
       data.config = config;
       data.auth0Lock = new Auth0Lock(
         config.auth0.clientId,
-        config.auth0.domain
-      );
+        config.auth0.domain,
+        {
+          auth: {
+            responseType: 'id_token token',
+          }
+        }
+      ); // new Auth0Lock
     }
 
     {
-      const accessToken = localStorage.getItem("userToken");
+      var accessToken = localStorage.getItem("accessToken");
       if (accessToken) {
         this.configureAuthenticatedRequests();
-        this.data.auth0Lock.getUserInfo(accessToken, (error, profile) => {
-          if (error) return alert("Auth0 error: " + error.message);
+        this.data.auth0Lock.getProfile(accessToken, (error, profile) => {
+          if (error) {
+            return alert("JOEW: Auth0 error: getting a profile:" + error.message);
+          }
           this.showUserAuthenticationDetails(profile);
         });
-      }
-    }
+      } // fi
+    } // const accesToken
 
     this.wireEvents();
-  },
+  }, //init initializer
 
+  // Only call when token is in localstorage
   configureAuthenticatedRequests() {
     $.ajaxSetup({
       beforeSend(xhr) {
         xhr.setRequestHeader(
+          'Authorization', 
+          'Bearer ' + localStorage.getItem('idToken') +' '+ localStorage.getItem('accessToken')
+        );
+        /*
+        xhr.setRequestHeader(
           "Authorization",
           "Bearer " + localStorage.getItem("userToken")
         );
-      }
-    });
-  },
+        */
+      } //xhr sending
+    }); //ajax
+  }, //configureAuthenticatedRequest()
 
+  // Only call when token is in localstorage
   showUserAuthenticationDetails(profile) {
     const showAuthenticationElements = !!profile;
 
@@ -73,42 +98,96 @@ window.userController = {
     }
   },
 
-  wireEvents() {
-    const { auth0Lock, config } = this.data;
 
+  wireEvents() {
+    var that = this;
+    const { auth0Lock, config } = this.data;
+    console.log("JOEW: auth0Lock.show being wired up event...")
+
+    // ORG auth0Lock.on location
+    auth0Lock.on("authenticated", function(authResult) {
+      console.log("JOEW: auth0Lock, ON activated");
+      auth0Lock.getUserInfo(authResult.accessToken, function(error, profileResult) {
+        console.log("JOEW: auth0Lock.getUser:");
+        if (error) {
+          // Handle error
+          //This should mean that if the profile failed to load, then the events should not be wired up
+          return alert('JOEW: Error, There was an error getting the profile: ' + err.message);
+        }
+        localStorage.setItem('accessToken', authResult.accessToken);
+        localStorage.setItem('idToken', authResult.idToken);
+        that.configureAuthenticatedRequests();
+        that.showUserAuthenticationDetails(profileResult);
+        accessToken = authResult.idToken;
+        profile = profileResult;
+    
+        // Update DOM
+      }); //getUserInfo
+    }); //auth0Lock.on
+
+    /*
     auth0Lock.on("authenticated", ({ accessToken }) =>
       auth0Lock.getUserInfo(accessToken, (error, profile) => {
         auth0Lock.hide();
         if (error) return alert("Auth0 error:" + error);
-
-        localStorage.setItem("userToken", accessToken);
+  
+        localStorage.setItem("idToken", idToken);
+        localStorage.setItem("accessToken", accessToken);
         this.configureAuthenticatedRequests();
         this.showUserAuthenticationDetails(profile);
       })
-    );
+    );  //auth0Lock.on    
+    */
+  
+  
 
     {
       const { loginButton, logoutButton, profileButton } = this.uiElements;
 
-      loginButton.click(() =>
-        auth0Lock.show({ auth: { params: { scope: "openid profile", responseType: 'id_token token' } } })
-      );
+      loginButton.click(() => {
+        
+        var params = {
+          authParams: {
+            scope: 'openid email user_metadata picture'
+          }
+        };
+        
+        that.data.auth0Lock.show( 
+          /*
+          {
+          auth: { 
+            params: { 
+              scope: "openid profile", 
+              responseType: 'id_token token' 
+            } 
+          } 
+          }
+          */
+        ); // auth0Lock.show      
+        console.log("JOEW: end step of click loginBtn")
+      }); // end loginButton.click
 
       logoutButton.click(() => {
-        localStorage.removeItem("userToken");
+        //localStorage.removeItem("userToken");
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("idToken");
+
         logoutButton.hide();
         profileButton.hide();
         loginButton.show();
-        auth0Lock.logout({ returnTo: "http://localhost:8100" });
-      });
+        //auth0Lock.logout({ returnTo: "http://localhost:8100" });
+        auth0Lock.logout();
+      }); // end logoutButton.click
 
       profileButton.click(() => {
         const url = config.apiBaseUrl + "/user-profile";
         $.get(url, data => {
-          $("#user-profile-raw-json").text(JSON.stringify(data, null, 2));
-          $("#user-profile-modal").modal();
-        });
-      });
-    }
-  }
-};
+          console.log("JOEW: Recieved a response data, to with alert function:" + data)
+          //$("#user-profile-raw-json").text(JSON.stringify(data, null, 2));
+          //$("#user-profile-modal").modal();
+          alert(JSON.stringify(data))
+        }) //.get
+      }); //end profileButton.click
+    } //es6
+  } //wire event
+} // user controller
---
+++ ch05-user-profile/index.JOE2.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Peter Sbarski
+ * Serverless Architectures on AWS
+ * http://book.acloud.guru/
+ * Last Updated: Feb 11, 2017
+ * Modified by Maxim Makatchev on January 7, 2018 
+ * sdfdsfdsfdsf
+ */
+
+'use strict';
+
+var jwt = require('jsonwebtoken');
+var request = require('request');
+ 
+exports.handler = function(event, context, callback){
+    if (!event.authToken) {
+        callback('Could not find authToken');
+        return;
+    }
+
+    var id_token = event.authToken.split(' ')[1];
+
+    console.log("JOE1:", id_token, "::", access_token, "::", event, "::", JSON.stringify(event), "::end");
+
+    var body = {
+        'id_token': id_token,
+        'access_token': access_token
+    };
+    var options = {
+        url: 'https://'+ process.env.DOMAIN + '/userinfo',
+        method: 'GET',
+        json: true,
+        body: body
+    };
+    console.log("JOE2:", body, "::", options, "::end");
+
+    var secretBuffer = new Buffer(process.env.AUTH0_SECRET);
+    //var secretBuffer = Buffer.from(process.env.AUTH0_SECRET);
+    jwt.verify(id_token, secretBuffer, function(err, decoded){
+        if(err){
+            console.log('JOE3: Failed jwt verification: ', err, 'auth: ', event.authToken);
+            callback('JOE3: Authorization Failed: ' + id_token + ", error: " + err + ", auth: " + event.authToken);
+        } else {
+            request(options, function(error, response, body){
+                console.log("JOEX1: " + JSON.stringify(response));
+                if (!error && response.statusCode === 200) {
+                    console.log("JOEX2: " + JSON.stringify(response));
+                    callback(null, body);
+                } else {
+                    callback(error);
+                }
+            });
+        }
+    })
+};
